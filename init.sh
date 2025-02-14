cd78b2ab087fc8ffe88b37bba626b383a7ce22cb
BuildTools <unconfigured@null.spigotmc.org>
2025-02-14 16:57:00 -0500

    maybe fix?

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+
 apt-get update && apt-get upgrade -y && apt-get install -y wget python3-pip ffmpeg p7zip-full
 
 pip install --upgrade pip
-pip install --no-cache-dir discord.py pyyaml requests yt-dlp asyncio aiohttp musicbrainzngs beautifulsoup4 aiofiles ffmpeg-python ffmpeg PyNaCl fuzzywuzzy python-Levenshtein numpy
-pip install --no-cache-dir stt
+pip install --no-cache-dir numpy<2
+pip install --no-cache-dir discord.py pyyaml requests yt-dlp asyncio aiohttp musicbrainzngs \
+    beautifulsoup4 aiofiles ffmpeg-python ffmpeg PyNaCl fuzzywuzzy python-Levenshtein
+
+pip install --no-cache-dir --force-reinstall stt
 
 REPO_URLS=("https://raw.githubusercontent.com/Angablade/DtownBeats/refs/heads/master" "https://angablade.com/stuff/dtownbeats")
 FILES=("bot3.py" "lyrics.py" "youtube_mp3.py" "youtube_pl.py" "voice_utils.py")
