a23702145b551d21e2e384ba33b2d76db9543cc1
BuildTools <unconfigured@null.spigotmc.org>
2025-03-30 01:40:26 -0400

    added headers

--- sources/apple_music_mp3.py
+++ sources/apple_music_mp3.py
@@ -11,7 +11,15 @@ class AppleMusicScraper:
         self.api_url = f"https://api.song.link/v1-alpha.1/links?Country=US&songIfSingle=true&url={url}"
 
     async def fetch_metadata(self):
-        headers = {"User-Agent": "Mozilla/5.0"}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Mode': 'navigate'
+        }
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(self.api_url, headers=headers) as response:
